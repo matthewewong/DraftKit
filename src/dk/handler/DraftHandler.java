dd3497264a17795bd4b52c51f75243b3a10ae299
MAVIRI <MAVIRI@MAVIRI-PC.home>
2015-05-08 20:29:59 -0400

    Fully completed the draft best player button; have not done anything with the taxi players yet when manually adding players.

--- src/dk/handler/DraftHandler.java
+++ src/dk/handler/DraftHandler.java
@@ -27,7 +27,8 @@ public class DraftHandler {
     public final String CONTRACT_S2 = "S2";
     public final String CONTRACT_X = "X";
     public final int SALARY = 1;
-    public final int MAX_PLAYER_SIZE = 23;
+    public final int MAX_STARTER_SIZE = 23;
+    public final int MAX_TAXI_SIZE = 8;
     
     public DraftHandler(DK_GUI gui, PlayerHandler playerHandler, StandingsHandler standingsHandler) {
         DraftDataManager ddm = gui.getDataManager();
@@ -57,16 +58,24 @@ public class DraftHandler {
     
     public void handleDraftBestPlayerRequest(DK_GUI gui, TableView<Team> standings) {
         boolean isDrafted = false;
+        boolean isStartersDone = false;
+        boolean isTaxisDone = false;
+        
         int i = 0;
         
         DraftDataManager ddm = gui.getDataManager();
         Draft draft = ddm.getDraft();
         ObservableList<Team> teams = draft.getTeams();
         Team luckyTeam = teams.get(i); //gets the first team
+        
+        //does the starting lineup
         for (; i < teams.size(); i++) {
             luckyTeam = teams.get(i);
-            if (luckyTeam.getStartingPlayers().size() == MAX_PLAYER_SIZE) {
+            if (luckyTeam.getStartingPlayers().size() == MAX_STARTER_SIZE) {
                 //DO NOTHING, INCREMENT THE COUNTER
+                if (i == teams.size() - 1) { //we reached the end of the starting lineup!
+                    isStartersDone = true;
+                }
             }
             else {
                 break; //break the for loop!
@@ -76,17 +85,47 @@ public class DraftHandler {
         //RESET i
         i = 0;
         
+        if (isStartersDone) {
+            //do the taxi lineup
+            for (; i < teams.size(); i++) {
+                luckyTeam = teams.get(i);
+                if (luckyTeam.getTaxiPlayers().size() == MAX_TAXI_SIZE) {
+                    //DO NOTHING, INCREMENT THE COUNTER
+                    if (i == teams.size() - 1) { //we reached the end of the draft!
+                        isTaxisDone = true;
+                    }
+                }
+                else {
+                    break; //break the for loop!
+                }
+            }
+        }
+        
+        if (isTaxisDone)
+            return;
+        
+        //RESET i
+        i = 0;
+        
         while (!isDrafted) {
             Player p = rankedValuedPlayers.get(i); //gets the next best player
             ObservableList<String> positions = p.getPositionsArray();  //get the player positions
             ObservableList<String> teamPositions = luckyTeam.getAvailablePositions(); //get the team positions
             for (int index = 0; index < positions.size(); index++) {
-                //search through the array of available positions, starting with the first one.
-                //if it is found in the available positions, add the player to the team.
-                if (teamPositions.indexOf(positions.get(index)) != -1) { //we found it!
-                    addPlayerToTeam(gui, draft, luckyTeam, p, positions.get(index), standings);
+                if (isStartersDone) {
+                    //starters lineup is done; accept everyone now!
+                    addPlayerToTaxiTeam(gui, draft, luckyTeam, p, positions.get(index), standings);
                     isDrafted = true;
                     break;
+                }
+                else {
+                    //search through the array of available positions, starting with the first one.
+                    //if it is found in the available positions, add the player to the team.
+                    if (teamPositions.indexOf(positions.get(index)) != -1) { //we found it!
+                        addPlayerToTeam(gui, draft, luckyTeam, p, positions.get(index), standings);
+                        isDrafted = true;
+                        break;
+                    }
                 }
             }
             //if we get here (out of the for loop), increment the player (next best player)
@@ -102,6 +141,21 @@ public class DraftHandler {
         player.setPickNumber(draftedPlayers.size() + 1);
         
         team.addStartingPlayer(player);
+        draft.removePlayer(player);
+        draftedPlayers.add(player);
+        rankedValuedPlayers.remove(player);
+        pH.initLists(gui);
+        sH.editStandingsTableContents(standings, draft);
+    }
+    
+    public void addPlayerToTaxiTeam(DK_GUI gui, Draft draft, Team team, Player player, String position, TableView<Team> standings) {
+        player.setFantasyTeam(team.getTeamName());
+        player.setTeamPosition(position);
+        player.setContract(CONTRACT_X);
+        player.setSalary(SALARY);
+        player.setPickNumber(draftedPlayers.size() + 1);
+        
+        team.addTaxiPlayer(player);
         draft.removePlayer(player);
         draftedPlayers.add(player);
         rankedValuedPlayers.remove(player);
